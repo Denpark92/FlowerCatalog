70be9471e12305f2659a65c212161442786c8742
Denis Tkachenko <denpark92@yandex.ru>
2017-12-11 23:20:52 +0300

    Добавлена стрелка наверх

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -76,6 +76,13 @@ AppAsset::register($this);
 <!--</footer>-->
 
 <?php $this->endBody() ?>
+<div id="toTop">
+    <svg class="top_arrow" version="1.1" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 128 128">
+        <path d="M122.3,96.6c5.3-5.8,4.8-14.8-0.8-20.3L74.2,28.9c-2.8-2.8-6.5-4.2-10.2-4.2s-7.4,1.4-10.2,4.2L6.5,76.2
+	c-5.5,5.5-6.1,14.6-0.8,20.3c5.6,6.1,15.1,6.3,20.9,0.5l30.3-30.3c3.9-3.9,10.2-3.9,14.1,0l30.3,30.3
+	C107.2,102.9,116.7,102.7,122.3,96.6z"/>
+    </svg>
+</div>
 </body>
 </html>
 <?php $this->endPage() ?>
--- web/js/main.js
+++ web/js/main.js
@@ -12,4 +12,16 @@ $(document).ready(function () {
             }
         }
     });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= 200) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+
+    $('#toTop').click(function() {
+        $("body,html").animate({scrollTop:0},800);
+    });
 });
